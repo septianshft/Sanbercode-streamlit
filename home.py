6ddaccc2f1a0081dfd71c8a4f32852571c2382e2
septianshft <septianhkc@gmail.com>
2024-12-21 11:49:39 +0700

    commit

--- home.py
+++ home.py
@@ -4,47 +4,42 @@ import streamlit as st
 def main():
     st.title("Curriculum Vitae")
 
-    # Foto
+    # Fotogit
     st.image("foto.png", width=150, caption="Foto Saya")  # Ganti 'foto.jpg' dengan file foto Anda
 
     # Biodata
     st.header("Biodata")
     st.write("""
     **Nama**: Septian Rizqi Arifandi 
-    **Tanggal Lahir**: 1 Januari 1990  
-    **Alamat**: Jl. Contoh No. 1, Jakarta  
-    **Email**: johndoe@example.com  
-    **Nomor Telepon**: +62 812-3456-7890  
+    **Tanggal Lahir**: 4 Januari 2002  
+    **Alamat**: East Pondok Kacang, South Tangerang City, Banten 
+    **Email**: septianhkc@gmail.com  
+    **Nomor Telepon**: +62 822-1325-2152  
     """)
 
     # Pendidikan
     st.header("Pendidikan")
     st.write("""
-    - **S1 Teknik Informatika** - Universitas ABC (2010-2014)  
-    - **SMA Negeri 1 Jakarta** (2007-2010)  
-    """)
-
-    # Pengalaman Kerja
-    st.header("Pengalaman Kerja")
-    st.write("""
-    - **Software Engineer** - PT Teknologi Maju (2015-2020)  
-      Pengembangan aplikasi berbasis web dan mobile.  
-    - **Data Analyst** - PT Data Insights (2020-Sekarang)  
-      Analisis data menggunakan Python dan SQL.  
+    - **S1 Teknik Industri** - Universitas Telkom (Under Graduate)  
+    - **Madrasah Aliyah Negri 4 Jakarta**  
     """)
 
     # Pengalaman Organisasi
     st.header("Pengalaman Organisasi")
     st.write("""
-    - **Ketua** - Himpunan Mahasiswa Teknik Informatika (2013-2014)  
-    - **Anggota** - Komunitas Data Science Indonesia (2020-Sekarang)  
+    - **Asisten** - Laboratorium Pengembanga Produk   
+    - **Asisten** - Penelitian Proyek Dosen 
+    - **Anggota** - Field Officer kepanitiaan orientasi kampus
+    - **Anggota** - Lembaga Zakat Universitas Telkom
+    - **Anggota** - Central Computer Improvemnet
     """)
 
     # Pengalaman Pelatihan
     st.header("Pengalaman Pelatihan")
     st.write("""
-    - Bootcamp Data Science - XYZ Academy (2019)  
-    - Pelatihan Machine Learning - ABC Institute (2021)  
+    - Bootcamp Fullstack Data Science - Sanber x ITB  
+    - Bootcamp Data Science - Sanbercode
+    - Sertifikasi SAP - Universitas Telkom
     """)
 
     # Skill
@@ -52,7 +47,7 @@ def main():
     st.write("""
     - Pemrograman: Python, JavaScript, SQL  
     - Analisis Data: Pandas, NumPy, Matplotlib  
-    - Framework: Streamlit, Flask, Django  
+    - Framework: Streamlit
     """)
 
 if __name__ == "__main__":
